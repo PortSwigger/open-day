a80e0832f00612c94268290f8d06043fc04fa5f7
Craig Aspinall <craig.aspinall@PSIT100304.local>
2022-02-22 08:40:15 +0000

    Added draft of completed code

---
+++ main.py
@@ -0,0 +1,79 @@
+import http.server
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import http.cookies
+from http.cookies import SimpleCookie
+from cgi import parse_header, parse_multipart
+from urllib.parse import parse_qs
+import ssl
+
+
+class MyServer(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        cookies = SimpleCookie(self.headers.get('Cookie'))
+        authenticated = 'authenticated' in cookies.keys() and cookies['authenticated'].value == 'true'
+
+        if '/secure' in self.path and not authenticated:
+            return self.send_error(401)
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        if self.path == '/login':
+            return self.do_login()
+        elif self.path == '/logout':
+            return self.do_logout()
+        else:
+            return self.send_error(404)
+
+
+    def parse_POST(self):
+        content_length = int(self.headers['content-length'])
+        form_data_as_bytes = parse_qs(self.rfile.read(content_length), keep_blank_values=1)
+
+        form_data_as_strings = {}
+
+        for key in form_data_as_bytes:
+            form_data_as_strings[key.decode()] = form_data_as_bytes[key][0].decode()
+
+        return form_data_as_strings
+
+
+    def do_login(self):
+        form_data = self.parse_POST()
+        if self.authenticate(form_data['username'], form_data['password']):
+            self.send_response(302)
+            self.send_header('Location', '/secure/')
+            self.send_header('Set-Cookie', 'authenticated=true')
+            return self.end_headers()
+        else:
+            return self.send_error(401)
+
+
+    def do_logout(self):
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.send_header('Set-Cookie', 'authenticated=false; expires=Thu, 01 Jan 1970 00:00:00 GMT')
+        return self.end_headers()
+
+
+    # Should return True if the user us authenticated or False if they are not
+    def authenticate(self, username, password):
+        return username = "user" and password == "1234"
+
+
+    def end_headers(self):
+        self.send_header('Cache-Control','no-cache, no-store, must-revalidate')
+        self.send_header('Pragma','no-cache')
+        self.send_header('Expires','0')
+        return SimpleHTTPRequestHandler.end_headers(self)
+
+
+if __name__ == '__main__':
+    server = HTTPServer(("localhost", 8080), MyServer)
+    server.socket = ssl.wrap_socket(server.socket, keyfile="key.pem", certfile='cert.pem', server_side=True)
+    print("Server started on https://localhost:8080")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
