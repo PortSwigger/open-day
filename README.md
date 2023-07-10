4300799bc2fd84c0cc3254869869d1caba1fcd2d
Hannah Law <hannah.law@portswigger.net>
2023-07-10 09:33:59 +0100

    Add additional-1 branch.

--- README.md
+++ README.md
@@ -1,29 +1,19 @@
-# Web Server Exercise
+# Additional Task 1
 
-The aim of this exercise is to familiarize yourself with HTTP requests and responses, and take a look at some basic authentication mechanisms for logging into websites.
-In this repository, you can find a basic implementation of a web server written in Python. It is incomplete, and will need the functionality described in the comments implemented.
+We've created 2 new methods on the server. One accepts a comment as sent by a user, and stores it on the server. The other lists all the comments that are currently stored in the system, and the name of the user who created it.
 
-As part of this, you will also get the chance to use our product, Burp Suite Professional.
+Feel free to compare the new `main.py` file with your version to see what changes have been made.
 
-## Steps
-1. Launch the web server, and access the root page through Burp's built-in browser.
-2. Attempt to log in with the username and password: `alice : 1234`
-   - What issues do you encounter at this point? Which method do you think you will need to implement first?
-3. Are we able to access the `/secure` endpoint when not logged in? How could we prevent this?
-   - When logging in, how do we identify to the web server that we are a valid user?
-   - How can we prevent access to the endpoint when not authenticated? Are there any error messages that we can use?
-4. Use Burp to add a fake cookie in Repeater to gain access to `/secure`
-5. When we log out from the site, how do we prevent our cookie from staying in our session?
-   - [Spoiler](https://stackoverflow.com/questions/5285940/correct-way-to-delete-cookies-server-side)!
+### Step 1
+Alter the secure page so that a logged in user has a way to send comments to the server. The new endpoint requires a `POST` request to the server, and the endpoint is `/comments`. It expects form data with a field `comment`, of which should be the text content that the user sent.
 
-## Additional:
-1. Open the "Additional" project.
-1. I'd like to be able to log in with the user `bob` as well.
-   - How does the web server identify individual users?
-2. Create a form to submit plaintext comments to the page.
-3. Be able to view comments.
-4. Is there a way that we can exploit this comment functionality to steal cookies from users?
-   - https://portswigger.net/web-security/cross-site-scripting#stored-cross-site-scripting
+You'll notice that when you send a comment to the server, the page refreshes, but you don't see the new comment being shown below to the `User comments will be shown below!` heading.
 
-### Completely done?
-Provide some styling to the site.
+You can verify that the comments are being set to the server by visiting `/comments` from your browser, it'll show you the data that is currently stored on the server.
+
+### Setp 2
+Now that we know the comments are being stored on the server, let's update the page to show all the comments that have been sent in.
+
+Inspect the html for the secure page, and write some JavaScript to `fetch` the comments stored on the server. This information can be retrieved from the server via a `GET` request to the `/comments` endpoint. 
+
+If you need help, search for JavaScript fetch, ask one of the helpers, or ask one of your colleagues!
--- main.py
+++ main.py
@@ -5,20 +5,54 @@ from http.cookies import SimpleCookie
 from cgi import parse_header, parse_multipart
 from urllib.parse import parse_qs
 import ssl
-
+import json
+import random
+import string
 
 class MyServer(SimpleHTTPRequestHandler):
+    comments = []
+    authenticated_users = {}
+
     def do_GET(self):
-        # Prevent unauthenticated access when the authenticated cookie is not set to 'true'
-        return SimpleHTTPRequestHandler.do_GET(self)
+        authenticated = self.get_cookie('authenticated') == 'true'
+        if '/secure' in self.path and not authenticated:
+            return self.send_error(401)
+        elif self.path == '/comments':
+            return self.get_comments()
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
         if self.path == '/login':
             return self.do_login()
         elif self.path == '/logout':
             return self.do_logout()
+        elif self.path == '/comments':
+            return self.save_comment()
         else:
             return self.send_error(404)
+
+    def get_comments(self):
+        self.set_headers_for_json_response()
+        self.wfile.write(json.dumps(self.comments).encode('utf-8'))
+
+    def save_comment(self):
+        authenticated = self.get_cookie('authenticated') == 'true'
+        if not authenticated:
+            return self.send_error(401)
+
+        session_id = self.get_cookie('sessionId')
+        if session_id == None:
+            return self.send_error(401)
+
+        user = self.authenticated_users.get(session_id, 'Unknown')
+
+        form_data = self.parse_POST()
+        
+        self.comments.append({'user': user, 'comment': form_data['comment']})
+        self.send_response(302)
+        self.send_header('Location', '/secure')
+        return self.end_headers()
 
 
     def parse_POST(self):
@@ -36,23 +70,30 @@ class MyServer(SimpleHTTPRequestHandler):
     def do_login(self):
         form_data = self.parse_POST()
         if self.authenticate(form_data['username'], form_data['password']):
+            session_id = ''.join(random.choices(string.ascii_lowercase, k=5))
+            self.authenticated_users[session_id] = form_data['username']
             self.send_response(302)
-            self.send_header('Location', '/secure/')
-            # send authenticated cookie
+            self.send_header('Location', '/secure')
+            self.send_cookie('authenticated=true')
+            self.send_cookie(f'sessionId={session_id}')
             return self.end_headers()
         else:
             return self.send_error(401)
 
 
     def do_logout(self):
-        # Clear the cookie and redirect to the login page
-        return None
+        session_id = self.get_cookie('sessionId')
+        self.authenticated_users.pop(session_id, None)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.send_cookie('authenticated=false; expires=Thu, 01 Jan 1970 00:00:00 GMT')
+        self.send_cookie(f'sessionId={session_id}; expires=Thu, 01 Jan 1970 00:00:00 GMT')
+        return self.end_headers()
 
 
+    # Should return True if the user us authenticated or False if they are not
     def authenticate(self, username, password):
-        # Check that the username and password match what we are expecting
-        # Should return True if the user is authenticated or False if they are not
-        return False
+        return (username == "alice" and password == "1234") or (username == "bob" and password == "5678")
 
 
     def send_cookie(self, cookie):
@@ -70,6 +111,11 @@ class MyServer(SimpleHTTPRequestHandler):
         self.send_header('Pragma','no-cache')
         self.send_header('Expires','0')
         return SimpleHTTPRequestHandler.end_headers(self)
+
+    def set_headers_for_json_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
 
 
 if __name__ == '__main__':
